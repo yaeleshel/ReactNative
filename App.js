4a5f558ca60f39ee1ba597d4ed2ed3141915927c
yael eshel <yael.eshel99@gmail.com>
2022-05-03 10:18:47 +0300

    insert

--- App.js
+++ App.js
@@ -1,20 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import { Header } from './Container/Header/Header';
 
 export default function App() {
+
+
+  const pressHamburger = () => {
+    debugger
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.wrapHeader}>
+        <TouchableOpacity onPress={pressHamburger}>
+          <Image style={styles.hamburger} source={require('./Container/Images/hamburger.png')} />
+        </TouchableOpacity>
+        <Image style={styles.icon} source={require('./Container/Images/maccabi.png')} />
+      </View>
+      <View >
+        <Text>
+          שלום אבי
+        </Text>
+        <Text style={styles.disclaimer}>
+          סיימת כרגע ביקור אצל הרופא, לנוחיותך, ריכזנו את כל המשימות שלך במרחק לחיצת כפתור
+        </Text>
+      </View>
       <StatusBar style="auto" />
-    </View>
+    </View >
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#F9FAFA',
+    alignItems: 'flex-end',
+    justifyContent: 'flex-start',
+    paddingTop: 60,
+    paddingHorizontal: 10
   },
+  wrapHeader: {
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    flexDirection: 'row-reverse'
+  },
+  hamburger: {
+    width: 30,
+    height: 30,
+    marginLeft: 20
+  },
+  icon: {
+
+  }
 });
---
+++ Container/Header/Header.js
@@ -0,0 +1,15 @@
+
+import { StyleSheet, Image, View, Text } from 'react-native';
+
+const Header = () => {
+  return (
+    <View>
+      {/* <Image source={require('../Images/hamburger.png')} /> */}
+      <Text>text</Text>
+    </View>
+  );
+}
+
+
+export default Header;
+
